4912e642e20bb0d70badae941d8ac94a445f247c
Sophie Shin <syshin4072@gmail.com>
2025-01-04 15:24:34 -0600

    added more links

--- api/server.js
+++ api/server.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 
 const app = express();
 app.use(cors({
-  origin: ["http://localhost:3000", "https://leafquad-frontend.vercel.app"],
+  origin: ["http://localhost:3000", "https://leafquad-frontend.vercel.app", "https://leafquad-frontend-mc94j8yg5-syshin402s-projects.vercel.app"],
   credentials: true,
 }));
 app.use(express.json());
