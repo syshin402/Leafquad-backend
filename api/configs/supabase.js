41b5d3b287ddbac3f93e45d81d91f43b02551211
Sophie Shin <syshin4072@gmail.com>
2025-01-04 09:45:06 +0900

    removed checking

--- api/configs/supabase.js
+++ api/configs/supabase.js
@@ -2,9 +2,6 @@ const { createClient } = require("@supabase/supabase-js");
 
 require("dotenv").config();
 
-if (!process.env.SUPABASE_URL || !process.env.SUPABASE_ANON_KEY) {
-  throw new Error("Missing required Supabase environment variables");
-}
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
